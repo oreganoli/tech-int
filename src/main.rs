1c750f00aa6979fb4f667de81663473758768f03
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-15 09:29:14 +0200

    serve form

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use axum::{response::Html, routing::*, Extension, Router};
+use tera::Tera;
+#[tokio::main]
+async fn main() {
+    let tera = Tera::new("templates/**/*").expect("Could not initialize Tera template engine.");
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(Extension(Arc::new(tera)));
+    // Listen on port 3000
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index(Extension(tera): Extension<Arc<Tera>>) -> Html<String> {
+    let text = tera
+        .render("index.html", &tera::Context::new())
+        .expect("Could not render HTML");
+    Html(text)
 }
